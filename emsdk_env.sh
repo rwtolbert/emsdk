a3a5cdcecff398f039f6a197ac633c0a888981e1
Bob Tolbert <bob@tolbert.org>
2020-08-19 19:40:41 -0600

    Rework emsdk_env.sh script for bash, zsh and ksh
    This version reworks the script to for 3 current
    working shells and makes it easier to add more if they
    can be made to work.
    
    This also allows the script to work if the emsdk
    directory or the emsdk_env.sh script itself are sym links.
    
    this also adds a new test in scripts directory that
    tests the 3 know shells, the 3 choices of path to the
    script (direct, symlinked dir, symlinked file) and using
    both the "source" and "." syntax.

--- emsdk_env.sh
+++ emsdk_env.sh
@@ -4,50 +4,61 @@
 # This script tries to find its location but
 # this does not work in every shell.
 #
-# It is known to work in bash and zsh
+# It is known to work in bash, zsh and ksh
 #
 # Do not execute this script without sourcing,
 # because it won't have any effect then.
 # That is, always run this script with
 #
-#     . ./emsdk_env.sh
+#     . /path/to/emsdk_env.sh
+#
 # or
-#     source ./emsdk_env.sh
+#
+#     source /path/to/emsdk_env.sh
 #
 # instead of just plainly running with
 #
 #     ./emsdk_env.sh
 #
 # which won't have any effect.
-if [ -z "$BASH_SOURCE" ]; then
-  D1=`dirname $0`
-  DIR=`cd $D1 && pwd`
-  S=`basename $0`
 
-  READLINK=`which readlink`
-  if [ -x "$READLINK" ] ; then
-    DLINK=`readlink $D1/$S`
-    if [ "$DLINK" != "" ] ; then
-      D1=`dirname $DLINK`
-      DIR=`cd $D1 && pwd`
+CURRENT_SCRIPT=
+DIR="."
+
+if [ -n "$BASH_SOURCE" ]; then
+  CURRENT_SCRIPT="$BASH_SOURCE"
+elif [ -n "$ZSH_VERSION" ]; then
+  CURRENT_SCRIPT="${(%):-%x}"
+elif [ -n "$KSH_VERSION" ]; then
+  CURRENT_SCRIPT=${.sh.file}
+fi
+
+if [ -n "$CURRENT_SCRIPT" ]; then
+  DIR=$(dirname "$CURRENT_SCRIPT")
+  # now work out actual DIR if this is part of a symlink
+  if [ -h "$CURRENT_SCRIPT" ]; then
+    SYMDIR=$(dirname "$(ls -l "$CURRENT_SCRIPT" | sed -n "s/.*-> //p")")
+    if [ -z "$SYMDIR" ]; then
+      SYMDIR="."
     fi
+    FULLDIR="$DIR/$SYMDIR"
+    DIR=$(cd "$FULLDIR" > /dev/null 2>&1; /bin/pwd)
+    unset SYMDIR
+    unset FULLDIR
   fi
-  unset S
-  unset D1
-  unset DLINK
-  unset READLINK
-  if [ ! -f "$DIR/emsdk.py" ]; then
-    echo "Error: unable to determine 'emsdk' directory. Perhaps you are using a shell or" 1>&2
-    echo "       environment that this script does not support." 1>&2
-    echo "One possible solution is to source this script while in the 'emsdk' directory." 1>&2
-    echo 1>&2
-    unset DIR
-    return
-  fi
-else
-  DIR="$(dirname "$BASH_SOURCE")"
+fi
+
+if [ ! -f "$DIR/emsdk.py" ]; then
+  echo "Error: unable to determine 'emsdk' directory. Perhaps you are using a shell or" 1>&2
+  echo "       environment that this script does not support." 1>&2
+  echo 1>&2
+  echo "A possible solution is to source this script while in the 'emsdk' directory." 1>&2
+  echo 1>&2
+  unset DIR
+  return
 fi
 
 # Force emsdk to use bash syntax so that this works in windows + bash too
 eval `EMSDK_BASH=1 $DIR/emsdk construct_env`
+unset CURRENT_SCRIPT
 unset DIR
